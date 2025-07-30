1fd6f168862c319dc561ff77d8ece5b376e76cef
gourab8389 <deyg6988@gmail.com>
2025-07-31 00:52:51 +0530

    feat: implement Layout component with Navbar and Sidebar integration

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import { Navbar } from "./Navbar";
+import { Sidebar } from "./Sidebar";
+
+export const Layout = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <div className="flex">
+        <Sidebar />
+        <main className="flex-1 lg:ml-64">
+          <div className="p-6">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
